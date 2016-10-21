2216f954b89a4f61d73224fea52dba3d42a38a73
Henry Doan <henrydoan96@gmail.com>
2016-10-20 22:33:09 -0600

    added assignment 7 and lab 8

---
+++ labs/src/lab8/FractionMain.java
@@ -0,0 +1,10 @@
+package lab8;
+
+public class FractionMain {
+
+	public static void main(String[] args) {
+		Fraction fract1 = new Fraction(1, 5);
+		System.out.println(fract1.getNumerator() + '/' + fract1.getdenomentor());
+	}
+
+}
