a0d7162b17a3ee95739a4cafcba4d0485c09db42
Henry Doan <henrydoan96@gmail.com>
2016-12-01 14:05:47 -0700

    a11

--- P11_Doan_Henry/src/minimap.java
+++ P11_Doan_Henry/src/minimap.java
@@ -1,33 +1,61 @@
-import java.awt.Dimension; 
-import java.awt.Graphics; 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+/**
+ * Shows the minimap and the given RGB value of the given point.
+ * 
+ * @author Henry Doan
+ */
 public class minimap extends JPanel {
 	
 	private static final long serialVersionUID = 1L; 
 	private ArrayList<BufferedImage> images; 
-	private int frameNumber; 
+	private int frameNumber;
+	
+	// have materials here
+	private BufferedImage Minimap;
 	
 	/** 
 	 * Construct a panel with a starter images  
 	 * @param img  
+	 * @throws IOException 
 	 */ 
 	
-	public minimap(BufferedImage img) {
-		images = new ArrayList<BufferedImage>();
-		images.add(img); frameNumber = 0; 
-		setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+	public minimap() {
+		
+		//try catch of the materail 
+		try {
+			Minimap = ImageIO.read(new File("./imgs/minimap.png"));
+			
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		JPanel minimapPanel = new JPanel();
+		
+		minimapPanel.setLayout(new GridLayout(Minimap.getWidth(), Minimap.getHeight()));	
+		
 	}
 	
+	
+		
+
+	
 	/**
-	 * Add img to the ArrayList of images  
+	 * Add image to the ArrayList of images  
 	 * @param img  
 	 */ 
 	
@@ -49,23 +77,40 @@ public class minimap extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); 
 		g.drawImage(images.get(frameNumber),  0,  0, null);
-		// System.out.println("redraw " + frameNumber);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// panel Name
 		JFrame frame = new JFrame("Mini Map");
-		BufferedImage image = null;
 		
-		try {                
-			image = ImageIO.read(new File("./imgs/sprite_0.png"));
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-		 frame.getContentPane().add(new minimap(image));
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     frame.setPreferredSize(new Dimension(300, 300));
-	     frame.pack();
-	     frame.setVisible(true);
+		//Getting the image for the grid
+		BufferedImage image = ImageIO.read(
+		         new File("./imgs/minimap.png"));
+		
+		//refactor
+		//getting the image for the  scroll bar
+		ImageIcon ii = new ImageIcon("./imgs/minimap.png");
+	    JScrollPane jsp = new JScrollPane(new JLabel(ii));
+	    frame.getContentPane().add(jsp);
+
+	    //see the set RGB value of the given point
+	    int w = image.getWidth();
+	    int h = image.getHeight();
+	    int[] dataBuffInt = image.getRGB(0, 0, w, h, null, 0, w); 
+
+	    Color c = new Color(dataBuffInt[100]);
+	    
+	    // RGB value 126, 206, 46 of plains and prints it out in the console.
+	    System.out.println(c.getRed());
+	    System.out.println(c.getGreen());
+	    System.out.println(c.getBlue());;
+	    
+	    
+	    
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setPreferredSize(new Dimension(300, 300));
+	    frame.pack();
+	    frame.setVisible(true);
 	}
 
 }
