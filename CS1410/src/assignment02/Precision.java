607e66272e528cc3720970a6f6457dffd16d0d43
Henry Doan <henrydoan96@gmail.com>
2017-01-25 18:26:23 -0700

    assignement 2

--- CS1410/src/assignment02/Precision.java
+++ CS1410/src/assignment02/Precision.java
@@ -5,10 +5,13 @@ package assignment02;
  * current is less than one billion increment by one.
  * 
  * At the beginning of the program, it seems like the program never finishes and the system out 
- * print when the loop is finished. With some debugging with the commented out code 1.6777216E7
+ * print when the loop is finished will never print. With some debugging with the commented out code 
+ * the number never actually reaches a billion and the highest number it reaches is 1.6777216E7. The reason
+ * is that a float is only holds 4 bytes of storage in the system so the maximum number is 1.6777216E7 and will
+ * never truly hit one billion.
  * 
  * @author  Henry Doan
- * @version January 23, 2017
+ * @version January 24, 2017
  */
 
 public class Precision {
@@ -19,6 +22,8 @@ public class Precision {
 		
 		while ( current < 1_000_000_000) {
 			current = current + 1;
+			
+			// true to see the largest number
 //			if (current > largestNumber) {
 //				largestNumber = current;
 //			}
