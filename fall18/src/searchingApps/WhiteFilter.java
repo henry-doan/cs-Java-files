ae9098307c49ea2ed6099b3b277f36b0fff3b0db
Henry Doan <henrydoan96@gmail.com>
2018-11-15 11:26:46 -0700

    finished white filter

---
+++ fall18/src/searchingApps/WhiteFilter.java
@@ -0,0 +1,24 @@
+package searchingApps;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+
+public class WhiteFilter {
+	private WhiteFilter(){ }
+	
+	public static void main(String[] args) {
+		SET<String> set = new SET<String>();
+
+		In in = new In(args[0]);
+		while(!in.isEmpty()) {
+			String word = in.readString();
+			set.add(word);
+		}
+		
+		while(!StdIn.isEmpty()) {
+			String word = StdIn.readString();
+			if(set.contains(word)) StdOut.println(word);
+		}
+	}
+
+}
