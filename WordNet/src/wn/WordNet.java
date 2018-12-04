524577e0c522f1079b288f349fd7185eead6cd93
Henry Doan <henrydoan96@gmail.com>
2018-12-04 10:25:27 -0700

    added corner cases

--- WordNet/src/wn/WordNet.java
+++ WordNet/src/wn/WordNet.java
@@ -1,15 +1,19 @@
 package wn;
 
-
 public class WordNet {
 	 /* 
 	  * Constructor takes the name of the two input files.
 	  * 
 	  * @params synsets - string of synssets file.
 	  *         hypernym - string of hypernyms file.
+	  * @throws NullPointerException -  if any argument is null or an empty string.
+	  *         IllegalArgumentException - if the input does not correspond to a rooted DAG.
 	  */
 	 public WordNet(String synsets, String hypernyms) {
-		 
+		 if (synsets == "" || synsets == null || hypernyms == null || hypernyms == "" ) {
+			 throw new NullPointerException();
+		 }
+			 
 	 }
 
 	 /*
@@ -28,8 +32,12 @@ public class WordNet {
 	  * @params word - a string of the checking.
 	  * @return true - if word is a noun.
 	  *         false - if word is not a noun.
+	  * @throws NullPointerException -  if any argument is null or an empty string.
 	  */
 	 public boolean isNoun(String word) {
+		 if (word == "" || word == null) {
+			 throw new NullPointerException();
+		 }
 		return false;
 		 
 	 }
@@ -40,8 +48,13 @@ public class WordNet {
 	  * @params nounA - string, the first noun we are comparing to.
 	  *         nounB - string, the second noun we are comparing to.  
 	  * @return  - the distance between the two nouns.
+	  * @throws NullPointerException -  if any argument is null or an empty string.
+	  *         IllegalArgumentException - unless both of the noun arguments are WordNet nouns.
 	  */
 	 public int distance(String nounA, String nounB) {
+		 if (nounA == "" || nounA == null || nounB == null || nounB == "" ) {
+			 throw new NullPointerException();
+		 }
 		return 0;
 		 
 	 }
@@ -54,8 +67,13 @@ public class WordNet {
 	  * @params nounA - string, the first common ancestor.
 	  *         nounB - string, the second common ancestor.     
 	  * @return  - the shortest ancestral path between the two nouns.
+	  * @throws NullPointerException -  if any argument is null or an empty string.
+	  *         IllegalArgumentException - unless both of the noun arguments are WordNet nouns.
 	  */
 	 public String sap(String nounA, String nounB) {
+		 if (nounA == "" || nounA == null || nounB == null || nounB == "" ) {
+			 throw new NullPointerException();
+		 }
 		return nounB;
 		 
 	 }
